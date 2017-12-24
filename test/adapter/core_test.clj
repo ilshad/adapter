d25f2839bd3724fd64a74e53a113a20c1f231c3f
Ilshad Khabibullin <astoon.net@gmail.com>
2017-12-24 17:54:36 +0700

    tests for multi-adapter

--- test/adapter/core_test.clj
+++ test/adapter/core_test.clj
@@ -3,37 +3,60 @@
             [clojure.spec.alpha :as s]
             [adapter.core :as adapter]))
 
-(s/def ::person (s/keys :req [::first-name ::last-name ::email]))
-(s/def ::text string?)
-(s/def ::response (s/keys :req-un [::status ::body]))
-(s/fdef person->text :args (s/cat :person ::person) :ret ::text)
-(s/fdef text->response :args (s/cat :text ::text) :ret ::response)
+(s/def ::text       string?)
+(s/def ::person     (s/keys :req [::first-name ::last-name ::email]))
+(s/def ::specialist (s/keys :req [::occupation]))
+(s/def ::response   (s/keys :req [::status ::body]))
+
+(s/fdef person->text
+  :args (s/cat :person ::person)
+  :ret ::text)
+
+(s/fdef person+specialist
+  :args (s/cat :person ::person :specialist ::specialist)
+  :ret ::person)
+
+(s/fdef text->response
+  :args (s/cat :text ::text)
+  :ret ::response)
+
+(def john {::first-name "John" ::last-name "Smith" ::email "user@sample.org"})
+(def john* (with-meta john {::adapter/hint ::person}))
+(def john** (assoc john ::adapter/hint ::person))
+
+(def engineer {::occupation :engineer})
+(def engineer* (with-meta engineer {::adapter/hint ::specialist}))
 
 (defn person->text [person]
   (str (::first-name person) " "
        (::last-name  person) ", "
        (::email      person)))
 
+(defn person+specialist [person specialist]
+  (merge person specialist))
+
 (defn text->response [text]
   {:status  200
    :body    text})
 
-(def john
-  {::first-name  "John"
-   ::last-name   "Smith"
-   ::email       "user@sample.org"})
-
 (use-fixtures :once
   (fn [tests]
-    (adapter/register #'person->text #'text->response)
+    (adapter/register #'person->text #'person+specialist #'text->response)
     (tests)))
 
-(deftest empty-adapter-test
+(deftest empty-adapters
   (is (= (adapter/adapt ::person john) john)))
 
-(deftest person->text-test
-  (is (= (adapter/adapt ::text (with-meta john {::adapter/hint ::person}))
+(deftest simple-adapters
+  (is (= (adapter/adapt ::text john*)
          "John Smith, user@sample.org")))
+
+(deftest multi-adapters
+  (is (= (adapter/adapt ::person john* engineer*)
+         {::first-name "John"
+          ::last-name "Smith"
+          ::email "user@sample.org"
+          ::occupation :engineer})))
 
 (comment
   (deftest person->response-test
